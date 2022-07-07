5dac428404333a6698ea10400f5d62d6f47ccbdb
adhammo <adhammohamedbusi@gmail.com>
2022-07-07 23:56:46 +0200

    Merged all

--- Assets/Scenes/Main Menu/Scene/MainMenu.cs
+++ Assets/Scenes/Main Menu/Scene/MainMenu.cs
@@ -9,9 +9,9 @@ public class MainMenu : MonoBehaviour
     
     public void Play()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1); 
-
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TarekScene"); 
     }
+    
     public void Quit()
     {
         Application.Quit();
